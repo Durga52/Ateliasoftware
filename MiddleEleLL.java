169d2d7b6f6f1a0658d31dfa5ba5803070e3e8dd
Durga52 <83329023+Durga52@users.noreply.github.com>
2021-04-28 20:38:15 +0530

    Update MiddleEleLL.java

--- MiddleEleLL.java
+++ MiddleEleLL.java
@@ -1,19 +1,27 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
+
 /**
  * MiddleElement in LL
  */
 public class MiddleEleLL {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            arrayList.add(i, scanner.nextInt());
-        }
-        scanner.close();
-        int len = arrayList.size();
-        int mid=len/2;
-        System.out.println(arrayList.get(mid));
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    for (int i = 0; i < n; i++) {
+      linkedList.add(i, scanner.nextInt());
     }
+    scanner.close();
+    int len = linkedList.size();
+    int mid = len / 2;
+    int count = 0;
+    for (Integer num : linkedList) {
+      if (count == mid) {
+        System.out.println("Middle element is " + num);
+        break;
+      }
+      count++;
+    }
+  }
 }
