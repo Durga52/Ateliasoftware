0dbf0e55f8f6cc9f7f6a0e2bdbccff2a32ff3b1e
Durga52 <83329023+Durga52@users.noreply.github.com>
2021-04-28 20:46:41 +0530

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
  * Reverse Words in a given sentence
  */
 public class ReverseWords {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        scanner.close(); 
-        String words[] = str.split("\\s");
-        String revStr= "";
-        for (int i = 0; i < words.length; i++) { 
-            revStr = " " + words[i] + revStr; 
-        } 
-        System.out.print("Reversed string : " + revStr);
+    static String reversedWord(String word) {
+    String revWord = "";
+
+    for (int i = word.length() - 1; i >= 0; i--) {
+      revWord += word.charAt(i);
+    } 
+
+    return revWord;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.nextLine();
+    scanner.close();
+    String words[] = str.split("\\s");
+    for (int i = 0; i < words.length; i++) {
+      words[i] = reversedWord(words[i]);
     }
+    System.out.print(String.join(" ", words));
+  }
 }
